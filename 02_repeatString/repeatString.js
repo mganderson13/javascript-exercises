4c02764e6a9f289086162d7ae557f94f9d723c1a
Marianne Anderson <mganderson13@gmail.com>
2024-09-16 12:00:48 -0500

    first three exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
 
+    let start = "";
+    for (let i = 0; i < num; i++) {
+        start += string;
+    }
+    return start;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+let reverseString = "";
 
-};
+let stringSplit = [];
+stringSplit = string.split("");
+reverseString = stringSplit.reverse().join("");
+
+return reverseString;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
