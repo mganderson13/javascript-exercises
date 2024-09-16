4b41dffb355b20a2468f51b68f91f11e2ca38401
Marianne Anderson <mganderson13@gmail.com>
2024-09-16 14:39:22 -0500

    removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+
+    if (toRemove) {
+        for (const arg of toRemove) {
+
+            let toRemoveIndex = array.indexOf(arg);
+
+            while (toRemoveIndex !== -1) {
+                array.splice(toRemoveIndex, 1);
+                toRemoveIndex = array.indexOf(arg);
+            }
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
