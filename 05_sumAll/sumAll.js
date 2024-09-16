77ce3f2fbd8c9e5a0f7a6359649be989f196dfd3
Marianne Anderson <mganderson13@gmail.com>
2024-09-16 14:54:45 -0500

    sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(arg1, arg2) {
 
+let sum = 0;
+if (arg1 < 0 || arg2 < 0 || !Number.isInteger(arg1) || !Number.isInteger(arg2)) {
+    return 'ERROR'
+}else if (arg1 < arg2) {
+    for (let i = arg1; i <= arg2; i++) {
+        sum += i;
+    }
+}else if (arg1 > arg2) {
+    for (let i = arg2; i <= arg1; i++) {
+        sum += i;
+    }
+}
+    return sum;
 };
 
 // Do not edit below this line
