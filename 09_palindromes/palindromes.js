1c5353c76785cdee88e2a2a48a3b110f7558989c
Marianne Anderson <mganderson13@gmail.com>
2024-09-17 12:21:07 -0500

    palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+let array = string.toLowerCase().split("");
+console.log("array: ", array);
+let character = "abcdefghijklmnopqrstuvwxyz1234567890";
+
+let newArray = array.filter((element) => character.includes(element));
+console.log("newArray ", newArray);
+
+let reverseArray = [...newArray].reverse();
+console.log("reverseArray ", reverseArray);
+
+let palindrome = reverseArray.join('') === newArray.join('');
+console.log("palindrome: ", palindrome);
+
+return palindrome;
 };
 
 // Do not edit below this line
