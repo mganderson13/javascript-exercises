74610a30b564fa01d4ef2b6e7a06463554e5cd27
Marianne Anderson <mganderson13@gmail.com>
2024-09-17 13:45:25 -0500

    fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
 
+let num = parseInt(number);
+
+if (num === 0) {
+    return 0;
+}else if (num < 0) {
+    return "OOPS";
+}
+
+    let first = 1;
+    let next = 0;
+for (let i = 2; i <= num; i++) {
+    let current = first + next;
+    next = first;
+    first = current;
+}
+
+return first;
 };
 
 // Do not edit below this line
