1e11d33fca90ceabf9364f42feeddb0add06ba7b
Marianne Anderson <mganderson13@gmail.com>
2024-09-17 10:27:59 -0500

    calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,60 @@
-const add = function() {
+const add = function(...addends) {
 	
-};
+  let sum = 0
 
-const subtract = function() {
-	
-};
+  for (const addend of addends) {
+    sum += addend;
+  }
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
+  return sum;
 
 };
 
-const power = function() {
+const subtract = function(minuend, subtrahend) {
+	
+  let difference = minuend - subtrahend;
+
+  return difference;
+};
+
+const sum = function(array) {
+	
+  let sum = 0
+
+ array.forEach((element) => sum += element);
+  
+
+  return sum;
+};
+
+const multiply = function(array) {
+
+ let product = 1
+ 
+ array.forEach((element) => product *= element);
+  
+
+  return product;
+};
+
+const power = function(a, b) {
+
+  let power = a**b;
+
+  return power;
 	
 };
 
-const factorial = function() {
+const factorial = function(num) {
 	
+  if (num === 0){
+    return 1;
+  }else {
+    for (let i = num-1; i > 0; i--){
+    num *= i;
+    }
+  }
+  return num;
 };
 
 // Do not edit below this line
