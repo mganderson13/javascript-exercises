c6d0261d905d8557ae319010377f91eea1980fa8
Marianne Anderson <mganderson13@gmail.com>
2024-09-17 08:29:05 -0500

    tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+
+  let celsiusTemp = Math.round(((temp - 32) * 5/9) * 10) / 10;
+
+  return celsiusTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+
+  let fahrTemp = Math.round((temp * 9/5 + 32) * 10) / 10;;
+
+  return fahrTemp;
+
 };
 
 // Do not edit below this line
