91cecbcd19b795e1177a69d32c9e3f3aae9ceb65
Marianne Anderson <mganderson13@gmail.com>
2024-09-17 08:05:53 -0500

    leapYear exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    let everyFour = year % 4 === 0;
+    let century = year % 100 === 0;
+    let leapCentury = year % 400 === 0;
+
+    if (everyFour && (!century || leapCentury)) {
+        return true;
+    }else {
+        return false;
+    }
 
 };
 
