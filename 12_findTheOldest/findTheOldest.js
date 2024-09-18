4848721d6616cd8de8aecb72fdac8b65b59ad65e
Marianne Anderson <mganderson13@gmail.com>
2024-09-18 08:34:18 -0500

    findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const getAge = function (death, birth) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
-};
+const findTheOldest = function(array) {
+    return array.reduce((oldest, current) => {
+        const oldestPerson = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+        const currentPerson = getAge(current.yearOfDeath, current.yearOfBirth);
 
+    return oldestPerson < currentPerson ? current : oldest;
+})
+
+}
 // Do not edit below this line
 module.exports = findTheOldest;
